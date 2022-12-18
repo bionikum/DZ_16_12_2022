7ae8fc8f998a5e24ee95f0b3e3b953e28458054e
bionikum <bionikum@yandex.ru>
2022-12-18 11:02:37 +0500

    Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+Console.Clear();
+Console.Write("Введите числа(через пробел): ");
+int[] N = Console.ReadLine().Split(" ").Select(n => int.Parse(n)).ToArray();
+int count = 0;
+ 
+for (int i = 0; i < N.Length; i++)
+{
+    if (N[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во положительных чисел: {count}");
